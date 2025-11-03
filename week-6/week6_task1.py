ec1a7be22775c16d5c07559cb51d49506ba28e91
Tauha Imran <161915837+tauhaimran@users.noreply.github.com>
2025-11-03 09:07:36 +0500

    renaming some folders

---
+++ week-6/week6_task1.py
@@ -0,0 +1,4 @@
+# Tauha Imran| Buildables AI Fellowship – Week 6 Task 1  
+# [LinkedIn](https://www.linkedin.com/in/tauha-imran-6185b3280/) 
+# [GitHub](https://github.com/tauhaimran)
+# [Portfolio](https://tauhaimran.github.io/)  
